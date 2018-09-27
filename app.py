de858342866b8f10b3cdd820fce9556352079a30
Ryan Aveo <ryan.aveo@gmail.com>
2018-09-26 18:41:42 -0700

    add specific environment variable names for reddit API

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import pandas as pd
 import datetime as dt
 from api import reddit
 
-clientID = os.environ.get("SONG_SCRAPE_CLIENT_ID")
-secret = os.environ.get("SONG_SCRAPE_SECRET")
 appName = "song-scrape"
-username = os.environ.get("REDDIT_USER")
-password = os.environ.get("REDDIT_PASS")
+
+
+# ENVIRONMENT VARIABLES
+redditClientID = os.environ.get("REDDIT_SONG_SCRAPE_CLIENT_ID")
+redditSecret = os.environ.get("REDDIT_SONG_SCRAPE_SECRET")
+redditUsername = os.environ.get("REDDIT_USER")
+redditPassword = os.environ.get("REDDIT_PASS")
 
 if __name__ == "__main__":
-	redditAPI = reddit.redditAPI(clientID, secret, appName, username, password)
+	redditAPI = reddit.redditAPI(redditClientID, redditSecret, appName, redditUsername, redditPassword)
+	redditAPI.print_info()
